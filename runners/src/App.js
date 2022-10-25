ba82e2184472e5638ad4ec5b08a53302460813fd
Obed Rayo <obedrayo2356@gmail.com>
2022-10-24 21:55:30 -0500

    Add App

--- runners/src/App.js
+++ runners/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Carrito from './components/cliente/Carrito'
+import Ventas from './components/admin/Ventas'
+import ListaProducto from './components/cliente/listproducts'
+import NuevoProducto from './components/admin/NuevoProducto'
 
 
 function App() {
   return (
     <div className ="App">
-      <p> Hello world!! </p>
+        <Carrito/>
+        <Ventas/>
+        <ListaProducto/>
+        <NuevoProducto/>
     </div>
   );
 }
 
-export  default App;
+
+export default App;
