74949659517592ad7e9a2f34acbc8b6a229e8fba
Obed Rayo <obedrayo2356@gmail.com>
2022-11-11 15:47:54 -0500

    Add comments

--- backendAdmin/server.js
+++ backendAdmin/server.js
@@ -11,17 +11,19 @@ app.use(express.json());
 
 const pathName = "/admin"
 
-//Getter list
+//Get lista productos
 app.get (pathName, async (req, res) => {
     console.log("Peticion get");
     res.send(await productosGet.productosGetExport())
 })
 
+//Get lista Ventas
 app.get (pathName + "/ventas", async (req, res) => {
     console.log("Peticion get");
     res.send(await productosGet.ventasGetExport())
 })
 
+//GetbyId productos
 app.get (pathName + "/:id", async (req, res) => {
     console.log("Peticion getById");
     let id = req.params.id
@@ -36,18 +38,21 @@ app.get (pathName + "/:id", async (req, res) => {
     
 })
 
+//post productos
 app.post (pathName, async (req, res) => {
     console.log("PeticionPost");
     let producto = await productosGet.productosSetExport(req.body)
     res.send({"mensaje":"Guardado exitoso", "producto":producto})
 }) 
 
+//Update datos
 app.put(pathName + '/:id', async (req, res) => {
     console.log("PeticionPut");
     let respuesta = await productosGet.updateProductosExport(req.params.id, req.body);
     res.send({"mensaje":"Actualizado exitoso", "respuesta":respuesta})
 })
 
+//Cambio de stock
 app.put(pathName + '/modstock/:id', async (req, res) => {
     console.log("PeticionPutStock");
     let respuesta = await productosGet.modificarStockExport(req.params.id, req.body);
@@ -55,6 +60,7 @@ app.put(pathName + '/modstock/:id', async (req, res) => {
 })
 
 
+//Delete productos
 app.delete (pathName + "/:id", async (req, res) => {
     console.log("Peticion delete")
     let productoEliminado = await productosGet.productosDeleteExport(req.params.id)
