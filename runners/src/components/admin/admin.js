077f1f29697de1bd97fb29909f04eabbfe041661
Obed Rayo <obedrayo2356@gmail.com>
2022-11-17 20:30:13 -0500

    Terminando todas las funcionalidades de admin

--- runners/src/components/admin/admin.js
+++ runners/src/components/admin/admin.js
@@ -5,16 +5,22 @@ import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Navigate } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Admin () {
 
-
   const [datosVentas, setDatosVentas] = useState([{}])
   const [datosProductos, setDatosProductos] = useState([{}])
+  const [datosProductoUnico, setDatosProductoUnico] = useState([{}])
+  const [modificarProducto, setModificarProducto] = useState({nombre:"", descripcion:"", precio:"", stock:""})
+
+  const url = "http://localhost:5000/admin"
+  let navigate = useNavigate();
 
   useEffect(
     ()=> {
-      fetch("http://localhost:5000/admin/ventas")
+      fetch(url+ "/ventas")
       .then(
         (response)=>(response.json())
       )
@@ -28,7 +34,7 @@ function Admin () {
 
    useEffect(
     ()=> {
-      fetch("http://localhost:5000/admin")
+      fetch(url)
       .then(
         (response)=>(response.json())
       )
@@ -40,8 +46,97 @@ function Admin () {
     },[]
    )
 
+
+   const deleteProducto = async (id) => {
+
+    let idEliminar = parseInt(id)
+
+    let response = await fetch(`${url}/${idEliminar}`, {
+      method: 'DELETE'
+    }
+    )
+    if (response.status === 200) {
+      alert("Producto eliminado")
+    }
+   }
+
+   const VerProductoById = (id) => {
+
+    useEffect(
+      () => {
+        fetch(url + "/${id}")
+      .then(
+        (response)=>(response.json())
+      )
+      .then (
+        (response) => {
+        setDatosProductoUnico(response)
+        }
+      )
+      .catch(
+        (error) => {
+          console.log(error)
+        }
+      )
+    }, []
+    )
+   }
+
+   const ModificarDBProducto = async (id, nombre, descripcion, precio, stock) => 
+   {
+
+    let idModificar = parseInt(id)
+
+    let productoModificar = {}
+    productoModificar.nombre = nombre
+    productoModificar.descripcion = descripcion
+    productoModificar.precio = parseInt(precio)
+    productoModificar.stock = parseInt(stock)
+
+          let response = await fetch(`${url}/${idModificar}`,
+          {
+            method: 'PUT',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type':'application/json'
+            },
+            body:JSON.stringify(productoModificar)
+          })
+
+          navigate('"http://localhost:3000/admin"');
+
+          if (response.status === 200){
+            alert("Producto modificado")
+          }
+  
+  }
+
    let suma = 0;
-    
+
+   const crearProducto = async (id, nombre, descripcion, precio, stock) => 
+  {
+    let productoNuevo = {}
+    productoNuevo._id = parseInt(id)
+    productoNuevo.nombre = nombre
+    productoNuevo.descripcion = descripcion
+    productoNuevo.precio = parseInt(precio)
+    productoNuevo.stock = parseInt(stock)
+
+
+    await fetch(url, 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(productoNuevo)
+    }
+    ).then(
+      (response) => (response.json())
+    ).then (
+      alert("producto creado")
+    )
+   }
     //Funciones para los botones
 
     function Volver () {
@@ -49,57 +144,121 @@ function Admin () {
         setListaProductos(listaProductos = []);
     }
 
+
     const ListarProductos = () => (
 
         //Se crea la presentación de los datos.
-        datosProductos.map((elem,idx)=>{
-            return(
-                <div className="productDiv">
-                        <img src={elem.image} className="productImg"></img>
-                        <div className="productPName"> {elem.nombre} </div>
-                        <div className="productPPrice"> Precio: $ {new Intl.NumberFormat("de-DE").format(`${elem.precio}`)} </div>
-                        <div className="productPStock"><p> Stock: {elem.stock} </p></div>
-                        <div className="button"><Button variant="warning">Modificar</Button></div>
-                </div>
-            )
-        })
+        <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th>Precio</th>
+            <th>Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          datosProductos.map((datos, index) => {
+            return (
+              <tr>
+                <td>{datos._id}</td>
+                <td>{datos.nombre}</td>
+                <td>{datos.descripcion}</td>
+                <td>$ {new Intl.NumberFormat("de-DE").format(`${datos.precio}`)}</td>
+                <td>{datos.stock}</td>
+              </tr>
+            );
+          }
+          
+        )
 
-    )
+          }
+        </tbody>
+      </Table>
+      )
+
 
     const InicioImagen = () => (
       <img src="../../img/estrategia.jpg" alt="hi" />
-      
+
       )
 
     const ModificarProductos = () => (
           <Form className="ingadmin">
             <Form.Group className="ingadmin" >
+              <Form.Label>ID</Form.Label>
+              <Form.Control  id="id" type="number" placeholder="" onChange={
+                (e) => {}
+              }/>
+            </Form.Group>
+            <Form.Group className="ingadmin">
               <Form.Label>Nombre</Form.Label>
-              <Form.Control type="cliente" placeholder="" />
+              <Form.Control id="nombre" name="nombre" type="text" placeholder="" onChange={
+                (e) => {}
+              }/>
             </Form.Group>
             <Form.Group className="ingadmin" >
               <Form.Label>Descripcion</Form.Label>
-              <Form.Control type="cliente" placeholder="" />
+              <Form.Control id="descripcion" type="text" placeholder="" onChange={
+                (e) => {}
+              }/>
             </Form.Group>
             <Form.Group className="ingadmin" >
               <Form.Label>Precio</Form.Label>
-              <Form.Control type="cliente" placeholder="$" />
+              <Form.Control id="precio" type="number" placeholder="$" onChange={
+                (e) => {}
+              }/>
             </Form.Group>
-            <Form.Group className="ingadmin" controlId="exampleForm.ControlInput1">
+            <Form.Group className="ingadmin">
               <Form.Label>Stock</Form.Label>
-              <Form.Control type="cliente" placeholder="" />
+              <Form.Control id="stock" type="number" placeholder="" onChange={
+                (e) => {}
+              }/>
             </Form.Group>
             <p> </p>
-            <Button className="botonadmin" variant="primary" type="submit">
+            <Button className="botonadmin" variant="primary" type="submit" onClick = { async() =>{
+              crearProducto(
+                document.getElementById("id").value, 
+                document.getElementById("nombre").value, 
+                document.getElementById("descripcion").value, 
+                document.getElementById("precio").value, 
+                document.getElementById("stock").value
+                )
+              }
+            }>
                 Agregar
             </Button>
-            <Button className="botonadmin" variant="primary" type="submit">
+            <Button className="botonadmin" variant="primary" type="submit" onClick = { async () => {
+              ModificarDBProducto(
+                document.getElementById("id").value, 
+                document.getElementById("nombre").value, 
+                document.getElementById("descripcion").value, 
+                document.getElementById("precio").value, 
+                document.getElementById("stock").value
+              )
+            }
+            }>
                 Modificar
             </Button>
-            <Button className="botonadmin" variant="primary" type="submit">
+            <Button className="botonadmin" variant="primary" type="button" onClick = { async () => {
+              VerProductoById(
+                document.getElementById("id").value
+              )
+            }
+            }>
+                Buscar
+            </Button>
+            <Button className="botonadmin" variant="primary" type="submit" onClick = { async () => {
+              deleteProducto(document.getElementById("id").value)
+            }
+            }>
                 Eliminar
             </Button>
           </Form>
+
+
 
     )
 
